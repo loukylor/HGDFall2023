5bdaea754c81dff5ba1da84ecb1ae66494f98034
Kyler Li <loukylor@gmail.com>
2023-11-24 23:13:22 -0800

    add landmine

---
+++ Assets/Scripts/Landmine.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace HGDFall2023
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class Landmine : MonoBehaviour
+    {
+        public float force = 10000;
+        public Animator animator;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            // Get root parent
+            Transform parent = collision.transform;
+            while (parent.parent != null)
+            {
+                parent = parent.parent;
+            }
+
+            // Check if root parent has the player tag
+            if (!parent.CompareTag("Player"))
+            {
+                // If it's not the player, then return
+                return;
+            }
+
+            // TODO: Add explosion sound
+            // Start explosion animation
+            animator.enabled = true;
+
+            // Add force in direction of explosion
+            collision.rigidbody.AddForce(
+                collision.GetContact(0).normal * -force
+            );
+            Debug.Log(collision.GetContact(0).normal);
+        }
+
+        // This will be called by the animator, and shouldn't be called manually
+        public void OnAnimationFinish()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
